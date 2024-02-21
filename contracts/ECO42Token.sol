7f758b471eadf895ffef23996f2de4ad3e61bb92
Antoine Labalette <labalette.antoine@gmail.com>
2024-02-21 10:54:33 +0100

    feat: store funding history

--- contracts/ECO42Token.sol
+++ contracts/ECO42Token.sol
@@ -17,6 +17,7 @@ contract ECO42Token is ERC20 {
 
     mapping(uint256 => Project) public projects;
     uint256 public projectCount = 0;
+    mapping(address => mapping(uint256 => uint256)) public funds; // Store how many each funder has given to each project
 
     function createProject(string memory _name, uint256 _amountNeeded) public {
         require(_amountNeeded > 0, "Amount needed should be greater than 0");
@@ -31,20 +32,31 @@ contract ECO42Token is ERC20 {
         projectCount++;
     }
 
-    function fundProject(uint256 _id) public payable {
+    function fundProject(uint256 _id, uint256 _amount) public payable {
         Project storage project = projects[_id];
-        uint256 _amount = msg.value;
 
         require(project.recipient != address(0), "Project does not exist");
         require(!project.funded, "Project already funded");
-        require(
-            project.amountRaised + _amount <= project.amountNeeded,
-            "Project already funded"
-        );
-        require(_amount > 0, "Amount cannot be zero");
 
-        _mint(msg.sender, _amount);
-        project.amountRaised += _amount;
+        uint256 fund = funds[msg.sender][_id];
+        if (_amount > fund) {
+            uint256 diff = _amount - funds[msg.sender][_id];
+            require(
+                project.amountRaised + diff <= project.amountNeeded,
+                "Project already funded"
+            );
+            require(msg.value == diff, "The difference mut be paid");
+            _mint(msg.sender, diff);
+            project.amountRaised += diff;
+            funds[msg.sender][_id] = _amount;
+        } else {
+            uint256 diff = fund - _amount;
+            _burn(msg.sender, diff);
+            project.amountRaised -= diff;
+            funds[msg.sender][_id] = _amount;
+            (bool sent, ) = msg.sender.call{value: diff}("");
+            require(sent, "Failed to send Ether");
+        }
 
         if (project.amountRaised == project.amountNeeded) {
             project.funded = true;
